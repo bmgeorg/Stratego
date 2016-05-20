3884b56f6146e538fd893c5d422cbac11542ba6d
Brett George <bmgeorg@clemson.edu>
2016-05-20 11:18:47 -0400

    Rudimentary click and drag

--- setup/BoardSetup.java
+++ setup/BoardSetup.java
@@ -1,20 +1,76 @@
 package setup;
 
+import java.awt.Dimension;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.SwingUtilities;
 
 public class BoardSetup extends JFrame implements CharacterPanelDelegate {
 	private static final long serialVersionUID = 1L;
 
+	private JLayeredPane pane;
+	
+	private CharacterPanel panel;
+	private JLabel selectedCharacterImage = null;
+	private model.Type selectedCharacterType = null;
+
 	BoardSetup() {
-		add(new CharacterPanel(this));
+
+		pane = new JLayeredPane();
+		pane.setPreferredSize(new Dimension(500, 700));
 		
+		panel = new CharacterPanel(this);
+		pane.add(panel);
+		Dimension size = panel.getPreferredSize();
+		panel.setBounds(0, 0, size.width, size.height);
+
 		this.setResizable(false);
+		this.add(pane);
 		this.pack();
+
+		MouseAdapter mouse = new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Point p = SwingUtilities.convertPoint(pane, e.getPoint(), panel);
+				panel.mousePressed(p.x, p.y);
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if(selectedCharacterImage != null) {
+					pane.remove(selectedCharacterImage);
+					selectedCharacterImage = null;
+					selectedCharacterType = null;
+					pane.repaint();
+				}
+			}
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if(selectedCharacterImage != null) {
+					selectedCharacterImage.setLocation(e.getPoint());
+				}
+			}
+		};
+		pane.addMouseListener(mouse);
+		pane.addMouseMotionListener(mouse);
 	}
 
-	@Override
 	public void characterSelected(ImageIcon image, model.Type type) {
 		System.out.println("Character selected " + type.toString());
+		selectedCharacterImage = new JLabel(image);
+		selectedCharacterType = type;
+
+		pane.add(selectedCharacterImage, JLayeredPane.DRAG_LAYER);
+
+		Point p = MouseInfo.getPointerInfo().getLocation();
+		SwingUtilities.convertPointFromScreen(p, this);
+		Dimension size = selectedCharacterImage.getPreferredSize();
+		selectedCharacterImage.setBounds(p.x, p.y, size.width, size.height);
 	}
 }
--- setup/CharacterPanel.java
+++ setup/CharacterPanel.java
@@ -18,27 +18,14 @@ public class CharacterPanel extends JPanel {
 	
 	private CharacterPanelDelegate delegate;
 	
-	private ImageIcon[][] images = new ImageIcon[6][2];
 	private Type[][] types = new Type[6][2];
+	// All indexed by Type ordinal
+	private ImageIcon[] images = new ImageIcon[12];
 	private int[] counts = new int[12];
 	private JLabel[] countLabels = new JLabel[12];
 
 	CharacterPanel(CharacterPanelDelegate delegate) {
 		this.delegate = delegate;
-		
-		String src = "src/images/";
-		images[0][0] = new ImageIcon(src + "marshal.png");
-		images[0][1] = new ImageIcon(src + "general.png");
-		images[1][0] = new ImageIcon(src + "colonel.png");
-		images[1][1] = new ImageIcon(src + "major.png");
-		images[2][0] = new ImageIcon(src + "captain.png");
-		images[2][1] = new ImageIcon(src + "lieutenant.png");
-		images[3][0] = new ImageIcon(src + "sergeant.png");
-		images[3][1] = new ImageIcon(src + "miner.png");
-		images[4][0] = new ImageIcon(src + "scout.png");
-		images[4][1] = new ImageIcon(src + "spy.png");
-		images[5][0] = new ImageIcon(src + "bomb.png");
-		images[5][1] = new ImageIcon(src + "flag.png");
 		
 		types[0][0] = Type.MARSHAL;
 		types[0][1] = Type.GENERAL;
@@ -52,6 +39,20 @@ public class CharacterPanel extends JPanel {
 		types[4][1] = Type.SPY;
 		types[5][0] = Type.BOMB;
 		types[5][1] = Type.FLAG;
+		
+		String src = "src/images/";
+		images[Type.MARSHAL.ordinal()] = new ImageIcon(src + "marshal.png");
+		images[Type.GENERAL.ordinal()] = new ImageIcon(src + "general.png");
+		images[Type.COLONEL.ordinal()] = new ImageIcon(src + "colonel.png");
+		images[Type.MAJOR.ordinal()] = new ImageIcon(src + "major.png");
+		images[Type.CAPTAIN.ordinal()] = new ImageIcon(src + "captain.png");
+		images[Type.LIEUTENANT.ordinal()] = new ImageIcon(src + "lieutenant.png");
+		images[Type.SERGEANT.ordinal()] = new ImageIcon(src + "sergeant.png");
+		images[Type.MINER.ordinal()] = new ImageIcon(src + "miner.png");
+		images[Type.SCOUT.ordinal()] = new ImageIcon(src + "scout.png");
+		images[Type.SPY.ordinal()] = new ImageIcon(src + "spy.png");
+		images[Type.BOMB.ordinal()] = new ImageIcon(src + "bomb.png");
+		images[Type.FLAG.ordinal()] = new ImageIcon(src + "flag.png");
 		
 		counts[Type.FLAG.ordinal()] = 1;
 		counts[Type.BOMB.ordinal()] = 6;
@@ -72,36 +73,29 @@ public class CharacterPanel extends JPanel {
 			for(int j = 0; j < 2; j++) {
 				final Type type = types[i][j];
 				Box box = new Box(BoxLayout.Y_AXIS);
-				JLabel img = new JLabel(images[i][j]);
+				JLabel img = new JLabel(images[type.ordinal()]);
 				img.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 				box.add(img);
 				countLabels[type.ordinal()] = new JLabel(String.valueOf(counts[type.ordinal()]));
 				box.add(countLabels[type.ordinal()]);
-				add(box);
-				
-				final int row = i;
-				final int col = j;
-				box.addMouseListener(new MouseListener() {
-					@Override
-					public void mouseClicked(MouseEvent e) {
-						if(counts[type.ordinal()] > 0) {
-							delegate.characterSelected(images[row][col], types[row][col]);
-						}
-					}
-					@Override
-					public void mousePressed(MouseEvent e) {}
-					@Override
-					public void mouseReleased(MouseEvent e) {}
-					@Override
-					public void mouseEntered(MouseEvent e) {}
-					@Override
-					public void mouseExited(MouseEvent e) {}
-				});
+				this.add(box);
 			}
 		}
 	}
 	
-	public void characterPlaced(Type type) {
+	public void mousePressed(int x, int y) {
+		int row = y*6/this.getHeight();
+		int col = x*2/this.getWidth();
+		if(row < 0 || row > 5 || col < 0 || col > 1) {
+			return;
+		}
+		Type type = types[row][col];
+		if(counts[type.ordinal()] > 0) {
+			delegate.characterSelected(images[type.ordinal()], type);
+		}
+	}
+	
+	public void decrementCount(Type type) {
 		counts[type.ordinal()]--;
 		countLabels[type.ordinal()].setText(String.valueOf(counts[type.ordinal()]));
 	}
